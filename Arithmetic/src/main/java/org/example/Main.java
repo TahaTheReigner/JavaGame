f5fc4053b21d09a6923dc620b66a3281e0b5f742
68907 <68907@etu.he2b.be>
2026-03-11 00:33:36 +0100

    arithmetic

---
+++ Arithmetic/src/main/java/org/example/Main.java
@@ -0,0 +1,24 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int x = 10;
+        int y = 2;
+        int z;
+
+        //z = x + y;
+        //z = x - y;
+        //z = x * y ;
+        //z = x / y;
+        //z = x % y;
+        x++;
+
+
+
+        System.out.println(x);
+
+
+
+    }
+}
